03a6b09ee118b429f982e54d6630bfad25ed81ab
jonlikesapples <jonlikesapples@gmail.com>
2018-11-18 21:51:05 -0800

    sql file containing populated room and user table, still need to popular other tables

---
+++ HotelProject/HotelProject/src/db/hotel_backup.sql
@@ -0,0 +1,189 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for osx10.13 (x86_64)
+--
+-- Host: localhost    Database: singaporeHotels
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `BOOKING`
+--
+
+DROP TABLE IF EXISTS `BOOKING`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `BOOKING` (
+  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `date_booking_start` datetime NOT NULL,
+  `date_booking_end` datetime NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BOOKING`
+--
+
+LOCK TABLES `BOOKING` WRITE;
+/*!40000 ALTER TABLE `BOOKING` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BOOKING` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BOOKINGARCHIVE`
+--
+
+DROP TABLE IF EXISTS `BOOKINGARCHIVE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `BOOKINGARCHIVE` (
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `date_booking_start` datetime NOT NULL,
+  `date_booking_end` datetime NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BOOKINGARCHIVE`
+--
+
+LOCK TABLES `BOOKINGARCHIVE` WRITE;
+/*!40000 ALTER TABLE `BOOKINGARCHIVE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BOOKINGARCHIVE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `HOTEL`
+--
+
+DROP TABLE IF EXISTS `HOTEL`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `HOTEL` (
+  `hotel_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `postal_code` int(11) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`hotel_id`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `HOTEL`
+--
+
+LOCK TABLES `HOTEL` WRITE;
+/*!40000 ALTER TABLE `HOTEL` DISABLE KEYS */;
+/*!40000 ALTER TABLE `HOTEL` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `room` (
+  `room_id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_number` int(11) NOT NULL,
+  `rate` int(11) NOT NULL,
+  PRIMARY KEY (`room_id`,`room_number`,`rate`),
+  UNIQUE KEY `unique_room_num` (`room_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=1025 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1001,432,199),(1005,519,99),(1020,523,139),(1016,532,99),(1021,535,129),(1017,619,100),(1015,623,199),(1002,635,119),(1003,639,129),(1006,645,119),(1024,681,99),(1023,691,129),(1018,719,149),(1019,723,199),(1022,735,99),(1004,832,119),(1014,923,299);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `TRANSACTION`
+--
+
+DROP TABLE IF EXISTS `TRANSACTION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `TRANSACTION` (
+  `trans_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Booking_id` int(11) NOT NULL,
+  PRIMARY KEY (`trans_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TRANSACTION`
+--
+
+LOCK TABLES `TRANSACTION` WRITE;
+/*!40000 ALTER TABLE `TRANSACTION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TRANSACTION` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `USER` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `bookings` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+INSERT INTO `USER` VALUES (1,'jon','wong','jonwong@email','jonwongpassword','123'),(2,'Vera','Wong','verawong@email.com','verawongpassword',NULL),(3,'Steven','Louie','stevenlouie@email.com','stevenlouiepassword',NULL),(4,'Rachel','Green','centralperk@email.com','rachellovesgunther',NULL),(5,'Joey','Tribbiani','drakeramoray@dayofourlives.com','iloverachel',NULL),(6,'Chandler','Bing','chanandlerbong@email.com','matthewperry',NULL),(7,'Ross','Geller','fossils@nyu.edu','davidschwimmer',NULL),(8,'Phoebe','Buffay','pbuffay@massage.com','lisakudrow',NULL),(9,'Monica','Geller','mgeller@email.com','ihatesmoking',NULL);
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-18 21:45:22
